53e18cd4a064bf8d841835582367f1abacb91ee9
Kayn2347 <163551379+Kayn2347@users.noreply.github.com>
2024-08-30 13:04:52 +0800

    Add files via upload

---
+++ Section-11-Intruduction-to-Data-Structures-main/Section-11-Intruduction-to-Data-Structures-main/Intruduction-to-Data-Structures-Public/main.py
@@ -0,0 +1,18 @@
+What you will learn in PART 2
+PART 2 will introduce the core data structures of the Python programming language. We will move past the basics of procedural programming and explore how we can use the Python built-in data structures such as lists, dictionaries, tuples and sets to perform increasingly complex data analysis.
+
+Topics that are covered:
+
+Data Structures
+
+Strings
+
+Lists
+
+Tuples
+
+Sets
+
+Dictionaries
+
+Real Projects related to covered topics
