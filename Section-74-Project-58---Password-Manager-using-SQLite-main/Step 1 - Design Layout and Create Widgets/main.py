5c4eff6207babbad9df5ce00b6bbb4c3d6a0650c
Kayn2347 <163551379+Kayn2347@users.noreply.github.com>
2024-10-02 13:16:53 +0800

    Add files via upload

---
+++ Section-74-Project-58---Password-Manager-using-SQLite-main/Step 1 - Design Layout and Create Widgets/main.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+
+class PasswordManagerGUI:
+   def __init__(self, master):
+       PasswordManagerGUI
+
+
+def main():
+    main_window = Tk()
+    password_manager = PasswordManagerGUI(main_window)
+    main_window.mainloop
